ee2c1d6abe8b0744ae4906c25fd99ee9ae454b13
Lext3r <a7l8e9x5@gmail.com>
2014-11-18 14:06:38 +0300

    Added some file IO methods for matrix

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,3 +1,33 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Matrix {
 
+    public int[][] readMatrix(int dimmension) throws FileNotFoundException {
+        int[][] matrix = new int[dimmension][dimmension];
+        Scanner in = new Scanner(new File("Matrix.txt"));
+        in.nextLine();
+        for (int i = 0; i < dimmension; i++) {
+            for (int j = 0; j < dimmension; j++) {
+                matrix[i][j] = in.nextInt();
+            }
+        }
+        return matrix;
+    }
+
+    public int readMatrixDimmension() throws FileNotFoundException {
+        Scanner sc = new Scanner (new File("Matrix.txt"));
+        return sc.nextInt();
+    }
+
+    public void printMatrix(int[][] matrix) throws IOException {
+        FileWriter out = new FileWriter(new File("NewMatrix.txt"));
+        for (int[] row : matrix) {
+            for (int element : row) {
+                out.write(element+" ");
+            }
+            out.write("\n");
+        }
+        out.close();
+    }
 }
