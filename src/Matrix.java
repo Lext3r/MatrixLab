4ea81cfbd2dffdb700d920073ff40357d345073e
Lext3r <a7l8e9x5@gmail.com>
2014-11-18 14:35:48 +0300

    Improved code view

--- src/Matrix.java
+++ src/Matrix.java
@@ -33,15 +33,15 @@ public class Matrix {
 
     public int[][] rotate(int[][] matrix, int dimmension){
         int tmp;
-        for(int i=0;i<dimmension/2;i++)
+        for(int i = 0; i < dimmension / 2; i++)
         {
-            for(int j=i;j<dimmension-1-i;j++)
+            for(int j = i; j < dimmension - 1 - i; j++)
             {
                 tmp = matrix[i][j];
-                matrix[i][j]=matrix[j][dimmension-1-i];
-                matrix[j][dimmension-1-i]     = matrix[dimmension-1-i][dimmension-1-j];
-                matrix[dimmension-1-i][dimmension-1-j] = matrix[dimmension-1-j][i];
-                matrix[dimmension-1-j][i]     = tmp;
+                matrix[i][j]=matrix[j][dimmension - 1 - i];
+                matrix[j][dimmension - 1 - i] = matrix[dimmension - 1 -i][dimmension - 1 - j];
+                matrix[dimmension - 1 - i][dimmension - 1 - j] = matrix[dimmension - 1 - j][i];
+                matrix[dimmension - 1 - j][i] = tmp;
             }
         }
         return matrix;
