a395c11a5c98c4e231d587a62ba9962888720fa7
Lext3r <a7l8e9x5@gmail.com>
2014-11-18 14:32:55 +0300

    Add method fot rotating matrix

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         Matrix matrix = new Matrix();
-        matrix.printMatrix(matrix.readMatrix(matrix.readMatrixDimmension()));
+        int dimmension = matrix.readMatrixDimmension();
+        int [][] inputMatrix = matrix.readMatrix(dimmension);
+        int [][]matrix90 = matrix.rotate(inputMatrix,dimmension);
+        matrix.printMatrix(matrix90);
+
     }
 }
