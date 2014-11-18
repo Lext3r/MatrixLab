6326dd5cdd0b1fcfac11811a5d00f90dac98ba49
Lext3r <a7l8e9x5@gmail.com>
2014-11-18 23:36:55 +0300

    Improve isLatinSquare method Add some commentary

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,17 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ *
+ */
 public class Main {
-
-    private int[][] matrix;
-
     public static void main(String[] args) throws IOException {
         Matrix matrix = new Matrix();
-        int dimmension = readMatrixDimmension();
+        int dimmension = matrix.readMatrixDimmension();
         int[][]inputMatrix = matrix.readMatrix(dimmension);
         int checkValue = matrix.primaryDiagonalSum(inputMatrix);
         System.out.println(matrix.isMagicSquare(inputMatrix, checkValue));
         System.out.println(matrix.isLatinSquare(inputMatrix));
     }
 
-    public static int readMatrixDimmension() throws FileNotFoundException {
-        Scanner sc = new Scanner (new File("Matrix.txt"));
-        return sc.nextInt();
-    }
 }
--- src/Matrix.java
+++ src/Matrix.java
@@ -1,23 +1,48 @@
 import java.io.*;
 import java.util.Scanner;
 
+/**
+ * Class included some methods for matrix
+ */
+
 public class Matrix {
 
+    /**
+     * Method for rotating matrix by 90 degrees counterclockwise
+     * @param matrix
+     * @return rotated matrix
+     */
     public int[][] rotate90(int[][] matrix) {
 
         return reverseMatrixColumn(transposeMatrix(matrix));
     }
 
+    /**
+     * Method for rotating matrix by 180 degrees counterclockwise
+     * @param matrix
+     * @return rotated matrix
+     */
     public int[][] rotate180(int[][] matrix){
 
         return reverseMatrixColumn(reverseMatrixRow(matrix));
     }
 
+    /**
+     * Method for rotating matrix by 270 degrees counterclockwise
+     * @param matrix
+     * @return rotated matrix
+     */
     public int[][] rotate270(int[][] matrix){
 
         return reverseMatrixRow(transposeMatrix(matrix));
     }
 
+    /**
+     * Read matrix from input file
+     * @param dimmension
+     * @return matrix
+     * @throws FileNotFoundException
+     */
     public int[][] readMatrix(int dimmension) throws FileNotFoundException {
 
         int[][] matrix = new int[dimmension][dimmension];
@@ -31,6 +56,11 @@ public class Matrix {
         return matrix;
     }
 
+    /**
+     * Write matrix into output file
+     * @param matrix
+     * @throws IOException
+     */
     public void printMatrix(int[][] matrix) throws IOException {
 
         FileWriter out = new FileWriter(new File("NewMatrix.txt"));
@@ -56,6 +86,11 @@ public class Matrix {
         return matrix;
     }
 
+    /**
+     * Flip matrix left to right
+     * @param matrix
+     * @return
+     */
     public int[][] reverseMatrixColumn(int[][] matrix){
 
         for(int col = 0;col < matrix[0].length; col++){
@@ -68,6 +103,11 @@ public class Matrix {
         return matrix;
     }
 
+    /**
+     * Flip matrix up to down
+     * @param matrix
+     * @return
+     */
     public int[][] reverseMatrixRow(int[][] matrix){
 
         for(int row = 0; row < matrix.length; row++){
@@ -80,6 +120,12 @@ public class Matrix {
         return matrix;
     }
 
+    /**
+     * Check if all of the row have the same elements sum
+     * @param matrix
+     * @param expectedSum
+     * @return
+     */
     boolean isRowElementsSumEqual(int [][] matrix, int expectedSum){
         for (int row = 0; row < matrix.length; row++) {
             int sum = 0;
@@ -93,6 +139,12 @@ public class Matrix {
         return true;
     }
 
+    /**
+     * Check if all of the row have the same elements sum
+     * @param matrix
+     * @param expectedSum
+     * @return
+     */
     boolean isColumnElementsSumEqual(int [][] matrix, int expectedSum){
         for (int col = 0; col < matrix.length; col++) {
             int sum = 0;
@@ -106,6 +158,11 @@ public class Matrix {
         return true;
     }
 
+    /**
+     * Find the matrix main diagonal elements sum
+     * @param matrix
+     * @return
+     */
     int primaryDiagonalSum(int[][] matrix){
         int sum = 0;
         for (int row = 0; row < matrix.length; row++) {
@@ -114,6 +171,11 @@ public class Matrix {
         return sum;
     }
 
+    /**
+     * Find the matrix antidiagonal elements sum
+     * @param matrix
+     * @return
+     */
     int secondaryDiagonalSum(int[][] matrix){
         int sum = 0;
         for (int row = 0; row < matrix.length; row++) {
@@ -122,6 +184,13 @@ public class Matrix {
         return sum;
     }
 
+    /**
+     * Magic square - arrangement of distinct numbers, in a square grid, where the numbers in each row,
+     * and in each column, and the numbers in the main and secondary diagonals, all add up to the same number.
+     * @param matrix
+     * @param checkValue
+     * @return
+     */
     String isMagicSquare(int[][] matrix, int checkValue){
         if ((isColumnElementsSumEqual(matrix,checkValue)) &&
                 (isRowElementsSumEqual(matrix, checkValue)) &&
@@ -132,23 +201,24 @@ public class Matrix {
         }
     }
 
+    /**
+     * Latin square is an n × n array filled with n different symbols,
+     * each occurring exactly once in each row and exactly once in each column.
+     * @param matrix
+     * @return
+     */
     String isLatinSquare(int[][] matrix){
-        if ((isColumnElementMultiplicationEqual(matrix)) && (isRowElementMultiplicationEqual(matrix))) {
+        if ((isColumnElementMultiplicationEqual(matrix, factorial(5))) &&
+                (isRowElementMultiplicationEqual(matrix, factorial(5)))) {
             return("OK!");
         } else {
             return ("No");
         }
     }
 
-    boolean isColumnElementMultiplicationEqual(int [][] matrix){
-        int col = 0;
+    boolean isColumnElementMultiplicationEqual(int [][] matrix, int checkValue){
         int sum = 1;
-        int checkValue=0;
-        for (int row = 0; row < matrix.length; row++) {
-            sum *= matrix[row][col];
-            checkValue = sum;
-        }
-        for (col = 1; col < matrix.length; col++) {
+        for (int col = 0; col < matrix.length; col++) {
             sum = 1;
             for (int row = 0; row < matrix.length; row++) {
                 sum *= matrix[row][col];
@@ -160,15 +230,9 @@ public class Matrix {
         return true;
     }
 
-    boolean isRowElementMultiplicationEqual(int [][] matrix){
-        int row = 0;
+    boolean isRowElementMultiplicationEqual(int [][] matrix, int checkValue){
         int sum = 1;
-        int checkValue=0;
-        for (int col = 0; col < matrix.length; col++) {
-            sum *= matrix[row][col];
-            checkValue = sum;
-        }
-        for (row = 1; row < matrix.length; row++) {
+        for (int row = 0; row < matrix.length; row++) {
             sum = 1;
             for (int col = 0; col < matrix.length; col++) {
                 sum *= matrix[row][col];
@@ -180,4 +244,12 @@ public class Matrix {
         return true;
     }
 
+    public int factorial(int num) {
+        return (num == 0) ? 1 : num * factorial(num - 1);
+    }
+
+    public static int readMatrixDimmension() throws FileNotFoundException {
+        Scanner sc = new Scanner (new File("Matrix.txt"));
+        return sc.nextInt();
+    }
 }
